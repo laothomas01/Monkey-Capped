99b1cb94a26e11aba1cf5110c1beeae7f5a79ce9
Thomas Lao <laothomas01@gmail.com>
2021-06-12 21:10:09 -0700

    HeadMonkeyRestored
    restored head monkey files

---
+++ Assets/HeadMonkeyScream.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMonkeyScream : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public SphereCollider sc;
+    Enemy e;
+    void Start()
+    {
+        sc = GetComponent<SphereCollider>();
+        e = new Enemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sc.radius = Random.Range(1f, 3f);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //affect the enemy
+        if (other.tag == "enemy")
+        {
+            e.setScared(true);
+            Debug.Log("SCARED:" + e.isScared());
+
+            //when enemy is scared, do an enemy behavior.
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //affect the enemy
+
+    }
+
+
+}
